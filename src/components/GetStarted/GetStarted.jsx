8c563224cc2449faad4c56330368758dbbbd629f
Davytun <davidakintunde433@gmail.com>
2025-04-16 11:25:49 +0100

    update

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -6,7 +6,7 @@ const GetStarted = () => {
     <div id="get-started" className="g-wrapper">
       <div className="paddings innerWidth g-container">
         <div className="flexColCenter inner-container">
-          <span className="primaryText">Get started with Altic-Attic</span>
+          <span className="primaryText">Get started with Altair Attic</span>
           <span className="secondaryText">
             Subscribe to Altair Attic Limited and Discover
             <br />
